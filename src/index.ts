1fe40182471325c3170def3f9d3ff27ce0c9abcf
VincentZyu <1830540513@qq.com>
2025-06-25 15:22:02 +0800

    awa

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,41 @@
 import { Context, Schema } from 'koishi'
+import {} from 'koishi-plugin-adapter-onebot';
 
 export const name = 'auto-emoji-onebot'
 
-export interface Config {}
+export interface Config {
+  reactUserIdList: string[];
+}
 
-export const Config: Schema<Config> = Schema.object({})
+export const Config: Schema<Config> = Schema.intersect([
 
-export function apply(ctx: Context) {
-  // write your plugin here
+  Schema.object({
+    reactUserIdList: Schema.array(String)
+    .default(["1830540513"]),
+  })
+
+])
+
+export function apply(ctx: Context, config: Config) {
+
+  ctx.on('message', async (session) => {
+
+    if (!config.reactUserIdList.includes(session.userId)) {
+      return;
+    }
+
+    await session.onebot._request(
+      "set_group_reaction",
+      {
+        "group_id": session.channelId,
+        "message_id": session.event.message.id,
+        "code": "324", 
+        "is_add": true
+      }
+    ).catch((err) => {
+      ctx.logger.error(`Failed to react with emoji: ${err.message}`);
+    })
+    
+  });
+
 }
