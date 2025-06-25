8f2daf0a761d9eab06b9a6070e10480a54e047fc
VincentZyu <1830540513@qq.com>
2025-06-25 15:32:26 +0800

    awa

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,21 @@ export const name = 'auto-emoji-onebot'
 
 export interface Config {
   reactUserIdList: string[];
+  verboseConsoleOutput: boolean;
 }
 
 export const Config: Schema<Config> = Schema.intersect([
 
   Schema.object({
     reactUserIdList: Schema.array(String)
-    .default(["1830540513"]),
-  })
+    .default(["1830540513"])
+    .description("")
+  }).description("基础设置"),
 
+  Schema.object({
+    verboseConsoleOutput: Schema.boolean()
+    .default(false),
+  }).description("debug"),
 ])
 
 export function apply(ctx: Context, config: Config) {
@@ -21,9 +27,23 @@ export function apply(ctx: Context, config: Config) {
   ctx.on('message', async (session) => {
 
     if (!config.reactUserIdList.includes(session.userId)) {
+      if (config.verboseConsoleOutput) {
+        ctx.logger.info(`用户id ${session.userId} 不在列表中，跳过。`);
+      }
       return;
     }
 
+    if (!session.onebot) {
+      if (config.verboseConsoleOutput) {
+        ctx.logger.error("当前会话不支持onebot协议。");
+      }
+      return;
+    }
+
+    if ( config.verboseConsoleOutput ) {
+      ctx.logger.info(`尝试对用户 ${session.userId} 的消息添加表情。消息内容: ${session.event.message.content.slice(0,10)}`);
+    }
+    
     await session.onebot._request(
       "set_group_reaction",
       {
@@ -33,7 +53,7 @@ export function apply(ctx: Context, config: Config) {
         "is_add": true
       }
     ).catch((err) => {
-      ctx.logger.error(`Failed to react with emoji: ${err.message}`);
+      ctx.logger.error(`添加表情失败: ${err.message}`);
     })
     
   });
